ca34716baf875f55edd117af19ee0b59ce690d2b
Albert <albertnadoyan98@gmail.com>
2024-12-29 18:39:51 +0400

    Created class POJO` Cat, and created new bean with scope.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,11 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld againBeanOne = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat firstCat = (Cat) applicationContext.getBean("cat");
+        Cat secondCat = (Cat) applicationContext.getBean("cat");
         System.out.println(bean.getMessage());
+        System.out.println(bean.equals(againBeanOne));
+        System.out.println(firstCat.equals(secondCat));
     }
 }
