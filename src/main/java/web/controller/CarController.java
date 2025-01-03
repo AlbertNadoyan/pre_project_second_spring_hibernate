1dee67e836a76711ecb50e46ae2c74f9aad92d91
Albert <albertnadoyan98@gmail.com>
2025-01-03 14:06:01 +0400

    Revert "Fix comment about logic which I wrote in controller."
    This reverts commit 6cd0986f89a7c22d86261b6cd0187e75450161a7.

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -6,7 +6,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import web.model.Car;
 import web.service.CarService;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/cars")
@@ -21,7 +24,13 @@ public class CarController {
 
     @GetMapping
     public String getCars(@RequestParam(name = "count", defaultValue = "5") int count, Model model) {
-        model.addAttribute("cars", carService.getCarsByCount(count));
+        List<Car> cars;
+        if(count >= 5){
+            cars = carService.getCars();
+        }else {
+            cars = carService.getCarsByCount(count);
+        }
+        model.addAttribute("cars", cars);
         return "cars";
 
     }
